0cf35b4dac5596e91387fa77cae51538bbcff429
Satya Prakash Yadav <yadavsatyaprakash689@gmail.com>
2025-06-01 13:55:43 +0530

    ..

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use((req,res,next)=>{
   next();
 });
 
-
+app.use("/", listingRouter);
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews",reviewRouter);
 app.use("/",userRouter);
